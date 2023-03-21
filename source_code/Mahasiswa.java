77fb2e64f2a81bfe81d7bddbec25d847e6097484
unknown <ihsanghozizulfikar@gmail.com>
2023-03-21 17:17:18 +0700

    a

---
+++ source_code/Mahasiswa.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author sekar
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String ras;
+    
+    public Mahasiswa(String nim, String nama, String nilai, String ras){
+        this.nim = nim;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.ras = ras;
+    }
+    
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+    
+    public void setRas(String ras) {
+        this.ras = ras;
+    }
+    
+    public String getNim() {
+        return this.nim;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public String getNilai() {
+        return this.nilai;
+    }
+    
+    public String getRas() {
+        return this.ras;
+    }
+}
